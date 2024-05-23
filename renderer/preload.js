cf9e6fcdb7248eb1f8d1c9be32a3d391b1b3e3da
Manjit Baishya <manjitbaishya15@gmail.com>
2024-05-23 18:34:49 +0530

    server updated

--- renderer/preload.js
+++ renderer/preload.js
@@ -3,7 +3,7 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('electronAPI', {
     fetchData: async () => {
         try {
-            const response = await fetch('http://192.168.1.22/data');
+            const response = await fetch('http://192.168.1.45/data');
             if (!response.ok) throw new Error('Network response was not ok');
             return await response.json();
         } catch (error) {
